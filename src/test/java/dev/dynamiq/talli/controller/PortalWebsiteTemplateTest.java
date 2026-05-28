2b35b500bb972c81b333e0343bb0ca7ac4503727
Shloimy Elbaum <shloimyelbaum@gmail.com>
2026-05-29 00:16:07 +0300

    allow more formparts

--- src/test/java/dev/dynamiq/talli/controller/PortalWebsiteTemplateTest.java
+++ src/test/java/dev/dynamiq/talli/controller/PortalWebsiteTemplateTest.java
@@ -2,7 +2,9 @@ package dev.dynamiq.talli.controller;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,5 +18,16 @@ class PortalWebsiteTemplateTest {
 
         assertThat(template).contains("enctype=\"multipart/form-data\"");
         assertThat(template).contains("_csrf=${_csrf.token}");
+    }
+
+    @Test
+    void tomcatPartLimitAllowsWebsiteEditorForm() throws Exception {
+        Properties properties = new Properties();
+        try (InputStream input = getClass().getResourceAsStream("/application.properties")) {
+            properties.load(input);
+        }
+
+        assertThat(Integer.parseInt(properties.getProperty("server.tomcat.max-part-count")))
+                .isGreaterThanOrEqualTo(500);
     }
 }
